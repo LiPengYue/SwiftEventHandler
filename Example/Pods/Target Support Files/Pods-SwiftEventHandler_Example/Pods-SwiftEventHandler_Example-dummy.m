6ad6f772e9bb4fcaa72cd64819e3f23b1366498e
liPengYue <yiersan@pengyue.li.net>
2019-01-21 11:16:43 +0800

    [事件传递工具]

---
+++ Example/Pods/Target Support Files/Pods-SwiftEventHandler_Example/Pods-SwiftEventHandler_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SwiftEventHandler_Example : NSObject
+@end
+@implementation PodsDummy_Pods_SwiftEventHandler_Example
+@end
---
+++ Example/Pods/Target Support Files/SwiftEventHandler/SwiftEventHandler-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_SwiftEventHandler : NSObject
+@end
+@implementation PodsDummy_SwiftEventHandler
+@end
